5d90249067bb7d9b47ad5d84ee7c229ddbb9ec9d
zinmyintnaung <zinmyintnaung@gmail.com>
2024-04-03 20:50:28 -0700

    okta config added

---
+++ 07-angular-ecommerce-v3/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '<USED_YOURS>',
+    issuer: '<USED_YOURS>',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
